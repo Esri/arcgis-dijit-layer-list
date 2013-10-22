e2666fcd7f4f96cea3958254734f5fab6aa3d76a
Matt Driscoll <mdriscoll@esri.com>
2013-10-22 16:01:18 -0700

    Use lang.mixin for widgets.

--- js/LayerLegend.js
+++ js/LayerLegend.js
@@ -49,18 +49,18 @@ function (
         // lifecycle: 1
         constructor: function(options, srcRefNode) {
             // mix in settings and defaults
-            declare.safeMixin(this.options, options);
+            var defaults = lang.mixin({}, this.options, options);
             // widget node
             this.domNode = srcRefNode;
             this._i18n = i18n;
             // properties
-            this.set("map", this.options.map);
-            this.set("layers", this.options.layers);
-            this.set("theme", this.options.theme);
-            this.set("visible", this.options.visible);
-            this.set("sublayers", this.options.sublayers);
-            this.set("zoomTo", this.options.zoomTo);
-            this.set("accordion", this.options.accordion);
+            this.set("map", defaults.map);
+            this.set("layers", defaults.layers);
+            this.set("theme", defaults.theme);
+            this.set("visible", defaults.visible);
+            this.set("sublayers", defaults.sublayers);
+            this.set("zoomTo", defaults.zoomTo);
+            this.set("accordion", defaults.accordion);
             // listeners
             this.watch("theme", this._updateThemeWatch);
             this.watch("visible", this._visible);
